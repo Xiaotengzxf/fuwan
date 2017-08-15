8df963a42837868ac24188bbc005cb57b7af03e2
Unknown <315082431@qq.com>
2017-08-15 21:03:25 +0800

    webview

--- FuWan/FuWan/Classes/WebViewController.swift
+++ FuWan/FuWan/Classes/WebViewController.swift
@@ -7,19 +7,50 @@
 //
 
 import UIKit
+import WebKit
+import WebViewBridge_Swift
 
-class WebViewController: UIViewController {
+class WebViewController: UIViewController, WKUIDelegate {
+    
+    var mWebView: WKWebView!
+    var strUrl : String?
+    var bridge : ZHWebViewBridge?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        //获取cookies并放入请求头部
+        let userContentController = WKUserContentController()
+        if let cookies = HTTPCookieStorage.shared.cookies{
+            print(cookies)
+            let script = getJSCookiesString(cookies: cookies)
+            let cookieScript = WKUserScript(source: script, injectionTime: WKUserScriptInjectionTime.atDocumentStart, forMainFrameOnly: false)
+            userContentController.addUserScript(cookieScript)
+        }
+        let webviewConfig = WKWebViewConfiguration()
+        webviewConfig.userContentController = userContentController
+        mWebView = WKWebView(frame: CGRect.zero, configuration: webviewConfig)
+        mWebView.translatesAutoresizingMaskIntoConstraints = false
+        mWebView.navigationDelegate = self
+        mWebView.uiDelegate = self
+        self.view.addSubview(mWebView)
+        
+        self.view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[webView]|", options: .directionLeadingToTrailing, metrics: nil, views: ["webView" : mWebView]))
+        self.view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[webView]|", options: .directionLeadingToTrailing, metrics: nil, views: ["webView" : mWebView]))
+        
+        if strUrl != nil && strUrl!.hasPrefix("http") {
+            let url = URL(string: strUrl!)
+            let request = URLRequest(url: url!)
+            mWebView.load(request)
+        }
+        
+        bridge = ZHWebViewBridge.bridge(mWebView!)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
     
 
     /*
@@ -31,5 +62,46 @@ class WebViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    public func getJSCookiesString(cookies: [HTTPCookie]) -> String {
+        var result = ""
+        let dateFormatter = DateFormatter()
+        dateFormatter.timeZone = NSTimeZone(abbreviation: "UTC") as TimeZone!
+        dateFormatter.dateFormat = "EEE, d MMM yyyy HH:mm:ss zzz"
+        
+        for cookie in cookies {
+            result += "document.cookie='\(cookie.name)=\(cookie.value); domain=\(cookie.domain); path=\(cookie.path); "
+            if let date = cookie.expiresDate {
+                result += "expires=\(dateFormatter.string(from: date)); "
+            }
+            if (cookie.isSecure) {
+                result += "secure; "
+            }
+            result += "'; "
+        }
+        return result
+    }
 
 }
+
+extension WebViewController : WKNavigationDelegate{
+    // 页面开始加载时调用
+    func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!){
+        
+    }
+    // 当内容开始返回时调用
+    func webView(_ webView: WKWebView, didCommit navigation: WKNavigation!){
+        
+    }
+    // 页面加载完成之后调用
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!){
+        /// 获取网页title
+        
+    }
+    // 页面加载失败时调用
+    func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error){
+        
+    }
+    
+}
+
